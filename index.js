eef8b9787120e6bc65beac580b4721023fab5ae4
NikolaStojicic <nikolastojicicsd@gmail.com>
2020-10-09 18:41:50 +0200

    backend for blog app

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
-const path = require("path");
-
+const cors = require("cors")
 const app = express();
-
-// Set static path
-app.use(express.static(path.join(__dirname, "client")));
-
+app.use(cors())
 app.use(bodyParser.json());
 
 const publicVapidKey =
   "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
+
+/*{
+    id: 1,
+    title: "Title1",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  },
+  {
+    id: 2,
+    title: "Title2",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  },
+  {
+    id: 3,
+    title: "Title3",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  },
+  {
+    id: 4,
+    title: "Title4",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  },
+  {
+    id: 5,
+    title: "Titl5",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  },
+  {
+    id: 6,
+    title: "Title6",
+    subtitle: "Subtitle1",
+    content: "Content1",
+    img: "no-img"
+  } */
+
+
+const arrayOfCards = []
+
+let _id = 0;
 
 webpush.setVapidDetails(
   "mailto:test@test.com",
@@ -20,16 +64,48 @@ webpush.setVapidDetails(
   privateVapidKey
 );
 
+app.get('/cards', (req, res) => {
+  res.json(arrayOfCards)
+})
+
+app.post('/card', (req, res) => {
+  const card = {
+    id: ++_id,
+    ...req.body
+  }
+  arrayOfCards.push(card)
+  res.json(card)
+})
+
+app.put('/card/:id', (req, res) => {
+  const itemId = req.params.id
+  const itemToChange = arrayOfCards.find(el => el.id === parseInt(itemId))
+  if (!itemToChange) {
+    res.json({
+      error: 'Item not found'
+    })
+  } else {
+    itemToChange.title = req.body.title
+    itemToChange.subtitle = req.body.subtitle
+    itemToChange.content = req.body.content
+    itemToChange.img = req.body.img
+
+    res.json(itemToChange)
+  }
+})
+
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
   // Get pushSubscription object
   const subscription = req.body;
-
+  console.log(subscription, 'payload')
   // Send 201 - resource created
   res.status(201).json({});
 
   // Create payload
-  const payload = JSON.stringify({ title: "Push Test" });
+  const payload = JSON.stringify({
+    title: "Push Test"
+  });
 
   // Pass object into sendNotification
   webpush
